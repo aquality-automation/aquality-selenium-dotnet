25cea09df9cbc38d8f7124e6fa6bc8706633ba77
mialeska <mialeska@users.noreply.github.com>
2019-07-31 16:10:34 +0300

    Feature/add java scripts (#24)
    * Add JavaScript files and reader. Resolves #3
    
    * implement ability to read custom javascript from the different assembly. Related to #21
    
    * add empty lines at the eof #21

--- Aquality.Selenium/src/Aquality.Selenium/Browsers/JavaScript.cs
+++ Aquality.Selenium/src/Aquality.Selenium/Browsers/JavaScript.cs
@@ -1,12 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Reflection;
 
 namespace Aquality.Selenium.Browsers
 {
     public enum JavaScript
     {
+        AutoAcceptAlerts,
+        BorderElement,
+        ClickElement,
+        ElementIsOnScreen,
+        GetCheckBoxState,
+        GetComboBoxText,
+        GetComboBoxValues,
+        GetElementByXPath,
+        GetElementText,
+        GetElementXPath,
+        GetTextFirstChild,
+        IsPageLoaded,
+        MouseHover,
+        ScrollBy,
+        ScrollToBottom,
+        ScrollToElement,
+        ScrollToElementCenter,
+        ScrollToTop,
         ScrollWindowBy,
-        GetElementXpath
+        SelectComboBoxValueByText,
+        SetFocus,
+        SetInnerHTML,
+        SetValue
+    }
+
+    public static class JavaScriptExtensions
+    {
+        private const string JavaScriptResourcePath = "Resources.JavaScripts";
+        private const string JavaScriptFileExtension = "js";
+        
+        public static string GetScript(this JavaScript javaScript)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            return javaScript.GetResourcePath().GetScript(assembly);
+        }
+
+        public static string GetScript(this string embeddedResourcePath, Assembly executingAssembly)
+        {
+            var resourcePath = $"{executingAssembly.GetName().Name}.{embeddedResourcePath}";
+            using (var stream = executingAssembly.GetManifestResourceStream(resourcePath))
+            {
+                if (stream == null)
+                {
+                    throw new InvalidOperationException(
+                        $"Assembly {executingAssembly.FullName} doesn't contain JavaScript at path {resourcePath} as EmbeddedResource.");
+                }
+
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        private static string GetResourcePath(this JavaScript javaScript)
+        {
+            return $"{JavaScriptResourcePath}.{javaScript}.{JavaScriptFileExtension}";
+        }
     }
 }
---
+++ Aquality.Selenium/tests/Aquality.Selenium.Tests/JavaScripts/JavaScriptTests.cs
@@ -0,0 +1,37 @@
+using Aquality.Selenium.Browsers;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Aquality.Selenium.Tests.JavaScripts
+{
+    [Parallelizable(ParallelScope.All)]
+    public class JavaScriptTests
+    {
+        private const string TestScriptPath = "Resources.TestJavaScript.js";
+#pragma warning disable IDE0052 // Remove unread private members
+        private static readonly JavaScript[] JavaScripts = Enum.GetValues(typeof(JavaScript)) as JavaScript[];
+#pragma warning restore IDE0052 // Remove unread private members
+
+        [TestCaseSource("JavaScripts")]
+        public void Should_GetJavaScript(JavaScript script)
+        {
+            Assert.IsNotEmpty(script.GetScript(), $"Failed to get javascript {script}");
+        }
+
+        [Test]
+        public void Should_GetCustomJavaScript()
+        {
+            Assert.IsNotEmpty(TestScriptPath.GetScript(Assembly.GetExecutingAssembly()), $"Failed to get javascript {TestScriptPath}");
+        }
+
+        [Test]
+        public void Should_BeUniqueJavaScripts()
+        {
+            Assert.IsEmpty(
+                JavaScripts.GroupBy(script => script.GetScript()).Where(group => group.Count() > 1).Select(group => string.Join(" and ", group)), 
+                "some duplicates where found among JavaScripts");
+        }
+    }
+}
