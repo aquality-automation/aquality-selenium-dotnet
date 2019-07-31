3299b40b2084dabb61e5ef910990693ae1a43742
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2019-07-31 11:20:42 +0300

    #3 Add summary to ElementSupplier

--- Aquality.Selenium/src/Aquality.Selenium/Elements/ElementSupplier.cs
+++ Aquality.Selenium/src/Aquality.Selenium/Elements/ElementSupplier.cs
@@ -3,5 +3,13 @@ using OpenQA.Selenium;
 
 namespace Aquality.Selenium.Elements
 {
+    /// <summary>
+    /// Delegate that defines constructor of element.
+    /// </summary>
+    /// <typeparam name="T">Type of element that has to implement IElement interface</typeparam>
+    /// <param name="locator">Element locator</param>
+    /// <param name="name">Element name</param>
+    /// <param name="state">Element state</param>
+    /// <returns>Element instance of type T</returns>
     public delegate T ElementSupplier<T>(By locator, string name, ElementState state) where T : IElement;
 }
