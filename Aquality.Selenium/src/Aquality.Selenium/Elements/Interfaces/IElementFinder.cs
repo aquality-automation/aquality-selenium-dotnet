37157a5dadf7a9cc9aa18395727da10b64e763a0
mialeska <mialeska@users.noreply.github.com>
2019-08-01 11:00:45 +0300

    Add IElementFinder implementation (#27)
    * Add IElementFinder to resolve #23

---
+++ Aquality.Selenium/src/Aquality.Selenium/Elements/Interfaces/IElementFinder.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.ObjectModel;
+
+namespace Aquality.Selenium.Elements.Interfaces
+{
+    /// <summary>
+    /// Provides ability to find elements in desired ElementState
+    /// </summary>
+    public interface IElementFinder : ISearchContext
+    {
+        /// <summary>
+        /// Finds elements in desired ElementState
+        /// </summary>
+        /// <param name="locator">elements locator</param>
+        /// <param name="timeout">timeout for search</param>
+        /// <param name="state">desired ElementState</param>
+        /// <returns>List of found elements</returns>
+        ReadOnlyCollection<IWebElement> FindElements(By locator, TimeSpan? timeout = null, ElementState state = ElementState.ExistsInAnyState);
+
+        /// <summary>
+        /// Finds element in desired ElementState
+        /// </summary>
+        /// <param name="locator">elements locator</param>
+        /// <param name="timeout">timeout for search</param>
+        /// <param name="state">desired ElementState</param>
+        /// <exception cref="OpenQA.Selenium.NoSuchElementException">Thrown if element was not found in time in desired state</exception> 
+        /// <returns>Found element</returns>
+        IWebElement FindElement(By locator, TimeSpan? timeout = null, ElementState state = ElementState.ExistsInAnyState);
+    }
+}
