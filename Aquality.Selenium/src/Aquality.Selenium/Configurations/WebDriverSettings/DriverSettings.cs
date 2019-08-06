ae8341ff16b00b87451d696727afe788579bae27
Alaksiej Mialeszka <mialeszkaalaksiej@gmail.com>
2019-08-06 13:59:26 +0300

    implement DriverSettings class #4

--- Aquality.Selenium/src/Aquality.Selenium/Configurations/WebDriverSettings/DriverSettings.cs
+++ Aquality.Selenium/src/Aquality.Selenium/Configurations/WebDriverSettings/DriverSettings.cs
@@ -1,6 +1,10 @@
-﻿using Aquality.Selenium.Utilities;
+﻿using Aquality.Selenium.Browsers;
+using Aquality.Selenium.Utilities;
 using OpenQA.Selenium;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using WebDriverManager.Helpers;
 
 namespace Aquality.Selenium.Configurations.WebDriverSettings
@@ -19,16 +23,76 @@ namespace Aquality.Selenium.Configurations.WebDriverSettings
             SettingsFile = settingsFile;
         }
 
+        private string DriverSettingsPath
+        {
+            get
+            {
+                return $".driverSettings.{BrowserName.ToString().ToLowerInvariant()}";
+            }
+        }
+
+        private IDictionary<string, object> BrowserCapabilities
+        {
+            get
+            {
+                return SettingsFile.GetObject<IDictionary<string, object>>($"{DriverSettingsPath}.driversettings");
+            }
+        }
+
+        protected IDictionary<string, object> BrowserOptions
+        {
+            get
+            {
+                return SettingsFile.GetObject<IDictionary<string, object>>($"{DriverSettingsPath}.options");
+            }
+        }
+
+        protected IList<string> BrowserStartArguments
+        {
+            get
+            {
+                return SettingsFile.GetObject<IList<string>>($"{DriverSettingsPath}.startArguments");
+            }
+        }
+
         protected JsonFile SettingsFile { get; }
 
-        public string WebDriverVersion => throw new NotImplementedException();
+        protected abstract BrowserName BrowserName { get; }
 
-        public Architecture SystemArchitecture => throw new NotImplementedException();
+        public abstract string DownloadDirCapabilityKey { get; }
 
         public abstract DriverOptions DriverOptions { get; }
 
-        public abstract string DownloadDir { get; }
+        public string WebDriverVersion => SettingsFile.GetObject<string>($"{DriverSettingsPath}.webDriverVersion");
 
-        public abstract string DownloadDirCapabilityKey { get; }
+        public Architecture SystemArchitecture
+        {
+            get
+            {
+                var jsonPath = $"{DriverSettingsPath}.systemArchitecture";
+                return SettingsFile.IsValuePresent(jsonPath)
+                ? (Architecture) Enum.Parse(typeof(Architecture), SettingsFile.GetObject<string>(jsonPath))
+                : Architecture.Auto;
+            }
+        }
+
+        public virtual string DownloadDir
+        {
+            get
+            {
+                if (BrowserOptions.ContainsKey(DownloadDirCapabilityKey))
+                {
+                    var pathInConfiguration = BrowserOptions[DownloadDirCapabilityKey].ToString();
+                    return pathInConfiguration.Contains(".") ? Path.GetFullPath(pathInConfiguration) : pathInConfiguration;
+                }
+
+                throw new InvalidDataException($"failed to find {DownloadDirCapabilityKey} profiles option for {BrowserName}");
+            }
+        }
+
+        protected void SetCapabilities(DriverOptions options)
+        {
+            BrowserCapabilities.ToList().ForEach(capability => options.AddAdditionalCapability(capability.Key, capability.Value));
+        }
     }
 }
