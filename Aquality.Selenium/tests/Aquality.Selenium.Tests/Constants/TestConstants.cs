7ea71b764a5af48928d3acf94a4a2a46f3d72160
Sergey Knysh <se.knysh@gmail.com>
2019-08-14 11:06:19 +0300

    #16 removed Automation practice

--- Aquality.Selenium/tests/Aquality.Selenium.Tests/Constants/TestConstants.cs
+++ Aquality.Selenium/tests/Aquality.Selenium.Tests/Constants/TestConstants.cs
@@ -1,12 +1,7 @@
 ﻿namespace Aquality.Selenium.Tests.Constants
 {
-    public class TestConstants
+    public static class TestConstants
     {
-        public const string UrlAutomationPractice = "http://automationpractice.com/index.php";
         public const string UrlTheInternet = "http://the-internet.herokuapp.com";
-
-        private TestConstants()
-        {
-        }        
     }
 }
