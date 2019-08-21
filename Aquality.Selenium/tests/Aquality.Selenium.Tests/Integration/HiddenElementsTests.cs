105381f1478623cc20b1c5e5b46f29d98fe5312c
Pavel Anihimovsky <47742067+pavelanihimovsky@users.noreply.github.com>
2019-08-21 16:35:54 +0300

    #79 Add tests for hidden elements and fix supplier resolver in ElementFactory (#81)

---
+++ Aquality.Selenium/tests/Aquality.Selenium.Tests/Integration/HiddenElementsTests.cs
@@ -0,0 +1,49 @@
+﻿using Aquality.Selenium.Browsers;
+using Aquality.Selenium.Elements;
+using Aquality.Selenium.Tests.Integration.TestApp;
+using Aquality.Selenium.Tests.Integration.TestApp.AutomationPractice.Forms;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Aquality.Selenium.Tests.Integration
+{
+    internal class HiddenElementsTests : UITest
+    {
+        private readonly SliderForm sliderForm = new SliderForm();
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            BrowserManager.Browser.GoTo(Constants.UrlAutomationPractice);
+        }
+
+        [Test]
+        public void Should_BePossibleTo_CheckThatHiddenElementExists()
+        {
+            Assert.IsTrue(sliderForm.GetAddToCartBtn(ElementState.ExistsInAnyState).State.IsExist);
+        }
+
+        [Test]
+        public void Should_BePossibleTo_CheckThatHiddenElementsExist()
+        {
+            var elements = sliderForm.GetListElements(ElementState.ExistsInAnyState, ElementsCount.MoreThenZero);
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(elements.Any());
+                Assert.IsTrue(elements.All(element => element.State.WaitForExist()));
+            });
+        }
+
+        [Test]
+        public void Should_BePossibleTo_CheckThatNotHiddenElementsNotDisplayed()
+        {
+            var elements = sliderForm.GetListElements(ElementState.Displayed, ElementsCount.MoreThenZero);
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(elements.Any());
+                Assert.IsFalse(elements.Any(element => element.State.WaitForDisplayed(TimeSpan.FromSeconds(1))));
+            });
+        }
+    }
+}
