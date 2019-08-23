f3dbaddf76919661425a76cd8ab809c04de262b5
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2019-08-23 13:43:55 +0300

    Add test for not displayed element which exists as part of #50

---
+++ Aquality.Selenium/tests/Aquality.Selenium.Tests/Integration/Usecases/ElementExistsButNotDisplayedTest.cs
@@ -0,0 +1,25 @@
+﻿using Aquality.Selenium.Browsers;
+using Aquality.Selenium.Elements;
+using Aquality.Selenium.Tests.Integration.TestApp;
+using Aquality.Selenium.Tests.Integration.TestApp.AutomationPractice.Forms;
+using Aquality.Selenium.Waitings;
+using NUnit.Framework;
+
+namespace Aquality.Selenium.Tests.Integration.Usecases
+{
+    internal class ElementExistsButNotDisplayedTest : UITest
+    {
+        [SetUp]
+        public void BeforeTest()
+        {
+            BrowserManager.Browser.GoTo(Constants.UrlAutomationPractice);
+        }
+
+        [Test]
+        public void Should_BePossibleTo_WaitForElement_WhichExistsButNotDisplayed()
+        {
+            var button = new SliderForm().GetAddToCartBtn(ElementState.ExistsInAnyState);
+            Assert.IsTrue(ConditionalWait.WaitFor(() => button.State.IsExist && !button.State.IsDisplayed));
+        }
+    }
+}
