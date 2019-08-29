59e77f088f8453c8fdf10cffbd125be4272de280
Pavel Anihimovsky <47742067+pavelanihimovsky@users.noreply.github.com>
2019-08-29 16:17:12 +0300

    Fix JS script as a part of #127 (#132)

--- Aquality.Selenium/src/Aquality.Selenium/Resources/JavaScripts/GetComboBoxTexts.js
+++ Aquality.Selenium/src/Aquality.Selenium/Resources/JavaScripts/GetComboBoxTexts.js
@@ -1,6 +1,6 @@
 return (function(element) {
-    var array = new Array();
-    for(i = 0; i < element.length; i++) {
+    let array = new Array();
+    for (let i = 0; i < element.length; i++) {
         array.push(element[i].text);
     }
     return array;
