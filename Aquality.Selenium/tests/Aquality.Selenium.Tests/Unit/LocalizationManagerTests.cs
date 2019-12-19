cab59d33b13dd66200a27d5b3ccc40aba32f5757
Sergey Knysh <se.knysh@gmail.com>
2019-12-19 10:36:37 +0300

    #158 added tests for localization message

---
+++ Aquality.Selenium/tests/Aquality.Selenium.Tests/Unit/LocalizationManagerTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Aquality.Selenium.Browsers;
+using NUnit.Framework;
+using Aquality.Selenium.Core.Localization;
+
+namespace Aquality.Selenium.Tests.Unit
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    internal class LocalizationManagerTests
+    {
+        private const string LocalizedNavigationMessage = "Navigate to url - 'test'";
+        private const string LogPath = "../../../Log/log.log";
+        private const string TestUrl = "test";
+        private const string NavigationKey = "loc.browser.navigate";
+        private static ILocalizedLogger LocalizedLogger => BrowserManager.GetRequiredService<ILocalizedLogger>();
+        private static ILocalizationManager LocalizationManager => BrowserManager.GetRequiredService<ILocalizationManager>();
+
+        [TestCase(LogLevel.Info)]
+        [TestCase(LogLevel.Debug)]
+        [TestCase(LogLevel.Error)]
+        [TestCase(LogLevel.Fatal)]
+        [TestCase(LogLevel.Warn)]
+        [Parallelizable(ParallelScope.None)]
+        public void Should_BeAble_LogLocalizedMessage(LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Info:
+                    LocalizedLogger.Info(NavigationKey, TestUrl);
+                    break;
+                case LogLevel.Debug:
+                    LocalizedLogger.Debug(NavigationKey, null, TestUrl);
+                    break;
+                case LogLevel.Error:
+                    LocalizedLogger.Error(NavigationKey, TestUrl);
+                    break;
+                case LogLevel.Fatal:
+                    LocalizedLogger.Fatal(NavigationKey, null, TestUrl);
+                    break;
+                case LogLevel.Warn:
+                    LocalizedLogger.Warn(NavigationKey, TestUrl);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, "Cannot process log level");
+            }
+
+            var logMessage = File.ReadAllLines(LogPath).LastOrDefault();
+            Assert.IsFalse(string.IsNullOrEmpty(logMessage), "Message should appear in log file");
+            Assert.IsTrue(logMessage.Contains(LocalizedNavigationMessage),
+                $"Message should be localized. Expected: {LocalizedNavigationMessage}, actual: {logMessage}");
+        }
+
+        [Test]
+        public void Should_BeAble_ToLocalizeLoggerMessage()
+        {
+            var message = LocalizationManager.GetLocalizedMessage("loc.browser.navigate", "test");
+            Assert.AreEqual(LocalizedNavigationMessage, message, "Message should be localized");
+        }
+
+        public enum LogLevel
+        {
+            Info,
+            Debug,
+            Error,
+            Fatal,
+            Warn
+        }
+    }
+}
