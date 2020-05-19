5e9c5c908432b5411ff5457f881a54d43d9e04ca
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-05-19 10:42:12 +0300

    Fix sonar issues

--- Aquality.Selenium/src/Aquality.Selenium/Browsers/Browser.cs
+++ Aquality.Selenium/src/Aquality.Selenium/Browsers/Browser.cs
@@ -249,7 +249,7 @@ namespace Aquality.Selenium.Browsers
         /// <param name="logKind">Type of logs <see cref="LogType"/></param>
         /// <returns>ReadOnlyCollection of log entries.</returns>
         /// <remark>
-        /// Does not work on current version of Selenium WebDriver <see cref="https://github.com/SeleniumHQ/selenium/issues/7323"/>
+        /// Does not work on current version of Selenium WebDriver. Issue: https://github.com/SeleniumHQ/selenium/issues/7323
         /// </remark>
         public ReadOnlyCollection<LogEntry> GetLogs(string logKind)
         {
