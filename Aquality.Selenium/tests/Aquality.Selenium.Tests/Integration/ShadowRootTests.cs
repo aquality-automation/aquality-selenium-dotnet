a0d32ac19c982ee787d96170e559625193aec39e
Alaksiej Mialeška <mialeska@users.noreply.github.com>
2022-01-28 00:35:29 +0300

    Update ShadowRootTests.cs

--- Aquality.Selenium/tests/Aquality.Selenium.Tests/Integration/ShadowRootTests.cs
+++ Aquality.Selenium/tests/Aquality.Selenium.Tests/Integration/ShadowRootTests.cs
@@ -19,7 +19,7 @@ namespace Aquality.Selenium.Tests.Integration
         {
             Assert.IsNotNull(form.ExpandShadowRoot(), "Should be possible to expand shadow root and get Selenium native ShadowRoot object");            
             Assert.IsNotNull(form.DownloadsToolbarLabel.GetElement(), "Should be possible do get the element hidden under the shadow");
-            Assert.IsNotNull(form.DownloadsToolbarLabel.FindElementInShadowRoot<ILabel>(ChromeDownoadsForm.NestedShadowRootLocator, "More actions menu").GetElement(),
+            Assert.IsNotNull(form.DownloadsToolbarLabel.FindElementInShadowRoot<ILabel>(ChromeDownloadsForm.NestedShadowRootLocator, "More actions menu").GetElement(),
                 "Should be possible to expand the nested shadow root and get the element from it");
             Assert.IsTrue(form.MainContainerLabel.State.IsDisplayed, "Should be possible to check that element under the shadow is displayed");
         }
@@ -29,7 +29,7 @@ namespace Aquality.Selenium.Tests.Integration
         {
             Assert.IsNotNull(form.ExpandShadowRootViaJs(), "Should be possible to expand shadow root and get Selenium native ShadowRoot object");
             Assert.IsNotNull(form.DownloadsToolbarLabelFromJs.GetElement(), "Should be possible do get the element hidden under the shadow");
-            Assert.IsNotNull(form.DownloadsToolbarLabelFromJs.JsActions.FindElementInShadowRoot<ILabel>(ChromeDownoadsForm.NestedShadowRootLocator, "More actions menu").GetElement(),
+            Assert.IsNotNull(form.DownloadsToolbarLabelFromJs.JsActions.FindElementInShadowRoot<ILabel>(ChromeDownloadsForm.NestedShadowRootLocator, "More actions menu").GetElement(),
                 "Should be possible to expand the nested shadow root and get the element from it");
             Assert.IsTrue(form.MainContainerLabelFromJs.State.IsDisplayed, "Should be possible to check that element under the shadow is displayed");
         }
