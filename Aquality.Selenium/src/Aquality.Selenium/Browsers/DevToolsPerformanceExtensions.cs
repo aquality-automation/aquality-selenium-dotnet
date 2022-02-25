138022a99bcb8e7ac5bdf90c75f2dd8809a1389d
Aleksey2 Meleshko <a2.meleshko@a1qa.com>
2022-02-25 21:28:53 +0100

    [DevTools] Performance monitoring +semver: feature

---
+++ Aquality.Selenium/src/Aquality.Selenium/Browsers/DevToolsPerformanceExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Linq;
+using OpenQA.Selenium.DevTools.V85.Performance;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aquality.Selenium.Browsers
+{
+    /// <summary>
+    /// Implementation of version-independent performance DevTools commands as extensions for <see cref="DevToolsHandling"/>.
+    /// For more information, see <see href="https://chromedevtools.github.io/devtools-protocol/tot/Performance/"/>.
+    /// </summary>
+    public static class DevToolsPerformanceExtensions
+    {
+        /// <summary>
+        /// Disable collecting and reporting metrics.
+        /// </summary>
+        /// <param name="devTools">Current instance of <see cref="DevToolsHandling"/>.</param>
+        /// <returns>A task for asynchronous command.</returns>
+        public static async Task DisablePerfomanceMonitoring(this DevToolsHandling devTools)
+        {
+            await devTools.SendCommand(new DisableCommandSettings());
+        }
+
+        /// <summary>
+        /// Enable collecting and reporting metrics.
+        /// </summary>
+        /// <param name="devTools">Current instance of <see cref="DevToolsHandling"/>.</param>
+        /// <param name="timeDomain">Time domain to use for collecting and reporting duration metrics.
+        /// Allowed Values: timeTicks, threadTicks. </param>
+        /// <returns>A task for asynchronous command.</returns>
+        public static async Task EnablePerfomanceMonitoring(this DevToolsHandling devTools, string timeDomain = null)
+        {
+            await devTools.SendCommand(new EnableCommandSettings { TimeDomain = timeDomain });
+        }
+
+        /// <summary>
+        /// Retrieve current values of run-time metrics.
+        /// </summary>
+        /// <param name="devTools">Current instance of <see cref="DevToolsHandling"/>.</param>
+        /// <returns>A task for asynchronous command with current values for run-time metrics as result.</returns>
+        public static async Task<IDictionary<string, double>> GetPerformanceMetrics(this DevToolsHandling devTools)
+        {
+            JToken result = await devTools.SendCommand(new GetMetricsCommandSettings());
+            return (result["metrics"] as JArray)
+                .ToDictionary(item => item["name"].ToString(), item => double.Parse(item["value"].ToString(), CultureInfo.InvariantCulture));
+        }
+    }
+}
