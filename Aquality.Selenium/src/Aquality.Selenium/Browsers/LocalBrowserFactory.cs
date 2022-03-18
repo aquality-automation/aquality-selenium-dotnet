69a826a58ae192335503e2552be73b48ac477e07
aPavar <a.pavar@yandex.ru>
2022-03-18 10:12:28 +0300

    [Feature/Enabling_Gecko_Driver_Host_Default_In_Settings] Add additional property to set geckoService.Host ::1 +semver: minor (#218)
    * [Feature/Enabling_Gecko_Driver_Host_Default_In_Settings] Add additional property to set geckoService.Host ::1
    
    * [Feature/Enabling_Gecko_Driver_Host_Default_In_Settings] Refactor code
    
    * [Feature/Enabling_Gecko_Driver_Host_Default_In_Settings] Refactor code

--- Aquality.Selenium/src/Aquality.Selenium/Browsers/LocalBrowserFactory.cs
+++ Aquality.Selenium/src/Aquality.Selenium/Browsers/LocalBrowserFactory.cs
@@ -24,6 +24,7 @@ namespace Aquality.Selenium.Browsers
     public class LocalBrowserFactory : BrowserFactory
     {
         private static readonly object WebDriverDownloadingLock = new object();
+        private const string HostAddressDefault = "::1";
 
         public LocalBrowserFactory(IActionRetrier actionRetrier, IBrowserProfile browserProfile, ITimeoutConfiguration timeoutConfiguration, ILocalizedLogger localizedLogger)
             : base(actionRetrier, browserProfile, timeoutConfiguration, localizedLogger)
@@ -49,7 +50,7 @@ namespace Aquality.Selenium.Browsers
                     case BrowserName.Firefox:
                         SetUpDriver(new FirefoxConfig(), driverSettings);
                         var geckoService = FirefoxDriverService.CreateDefaultService();
-                        geckoService.Host = "::1";
+                        geckoService.Host = ((FirefoxSettings)driverSettings).IsGeckoServiceHostDefaultEnabled ? HostAddressDefault : geckoService.Host;
                         driver = GetDriver<FirefoxDriver>(geckoService, (FirefoxOptions)driverSettings.DriverOptions, commandTimeout);
                         break;
                     case BrowserName.IExplorer:
