9bcee92ac493e2fe26be74c9e49b83d5e5e9ae7e
Aliaksej Mialeshka <a2.meleshko@a1qa.com>
2024-01-25 16:07:44 +0100

    Update ElementFactory to use Generate CSS locator logic in GenerateLocator method instead of GenerateXPathLocator

--- Aquality.Selenium/src/Aquality.Selenium/Elements/ElementFactory.cs
+++ Aquality.Selenium/src/Aquality.Selenium/Elements/ElementFactory.cs
@@ -114,6 +114,26 @@ namespace Aquality.Selenium.Elements
         }
 
         /// <summary>
+        /// Generates locator for target element
+        /// </summary>
+        /// <param name="baseLocator">locator of parent element</param>
+        /// <param name="webElement">target element</param>
+        /// <param name="elementIndex">index of target element</param>
+        /// <returns>target element's locator</returns>
+        protected override By GenerateLocator(By baseLocator, IWebElement webElement, int elementIndex)
+        {
+            try
+            {
+                return GenerateXpathLocator(baseLocator, webElement, elementIndex);
+            }
+            catch (WebDriverException ex)
+            {
+                return By.CssSelector(ConditionalWait.WaitFor(driver => driver.ExecuteJavaScript<string>(
+                    JavaScript.GetElementCssSelector.GetScript(), webElement), message: $"{ex.Message}. CSS selector generation failed too."));
+            }
+        }
+
+        /// <summary>
         /// Generates xpath locator for target element
         /// </summary>
         /// <param name="baseLocator">locator of parent element</param>
@@ -122,24 +142,16 @@ namespace Aquality.Selenium.Elements
         /// <returns>target element's locator</returns>
         protected override By GenerateXpathLocator(By baseLocator, IWebElement webElement, int elementIndex)
         {
-            try
+            if (IsLocatorSupportedForXPathExtraction(baseLocator))
             {
-                if (IsLocatorSupportedForXPathExtraction(baseLocator))
+                var locator = base.GenerateXpathLocator(baseLocator, webElement, elementIndex);
+                if (ElementFinder.FindElements(locator).Count == 1)
                 {
-                    var locator = base.GenerateXpathLocator(baseLocator, webElement, elementIndex);
-                    if (ElementFinder.FindElements(locator).Count == 1)
-                    {
-                        return locator;
-                    }
+                    return locator;
                 }
-                return By.XPath(ConditionalWait.WaitFor(driver => driver.ExecuteJavaScript<string>(
-                    JavaScript.GetElementXPath.GetScript(), webElement), message: "XPath generation failed"));
             }
-            catch (WebDriverException ex)
-            {
-                return By.CssSelector(ConditionalWait.WaitFor(driver => driver.ExecuteJavaScript<string>(
-                    JavaScript.GetElementCssSelector.GetScript(), webElement), message: $"{ex.Message}. CSS selector generation failed too."));
-            }                
+            return By.XPath(ConditionalWait.WaitFor(driver => driver.ExecuteJavaScript<string>(
+                JavaScript.GetElementXPath.GetScript(), webElement), message: "XPath generation failed"));
         }
 
         /// <summary>
