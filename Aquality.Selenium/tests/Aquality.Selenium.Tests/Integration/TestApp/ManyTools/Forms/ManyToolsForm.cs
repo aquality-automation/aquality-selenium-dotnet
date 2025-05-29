93d86825e70155ed0732f35fc1a67950760e001b
Aliaksej Mialeshka <a2.meleshko@a1qa.com>
2025-05-29 11:45:25 +0200

    Accept cookies in manytools site

--- Aquality.Selenium/tests/Aquality.Selenium.Tests/Integration/TestApp/ManyTools/Forms/ManyToolsForm.cs
+++ Aquality.Selenium/tests/Aquality.Selenium.Tests/Integration/TestApp/ManyTools/Forms/ManyToolsForm.cs
@@ -10,11 +10,12 @@ namespace Aquality.Selenium.Tests.Integration.TestApp.ManyTools.Forms
 {
     internal abstract class ManyToolsForm<T> : Form where T : ManyToolsForm<T>
     {
-        private const string BaseUrl = "https://manytools.org/";
+        private const string BaseUrl = "https://manytools.org/";        
 
         protected ManyToolsForm(By locator, string name) : base(locator, name)
         {
         }
+        private IButton AgreeButton => ElementFactory.GetButton(By.XPath("//button[@mode='primary']"), "Agree");
         private ILabel ValueLabel => FormElement.FindChildElement<ILabel>(By.XPath(".//code"), Name);
 
         protected abstract string UrlPart { get; }
@@ -29,6 +30,12 @@ namespace Aquality.Selenium.Tests.Integration.TestApp.ManyTools.Forms
             {
                 AqualityServices.Browser.GoTo(Url);
                 AqualityServices.Browser.WaitForPageToLoad();
+                State.WaitForDisplayed();
+                if (AgreeButton.State.IsDisplayed)
+                {
+                    AgreeButton.Click();
+                    AgreeButton.State.WaitForNotDisplayed();
+                }
             }, new List<Type> { typeof(WebDriverTimeoutException) });
 
             return (T)this;
