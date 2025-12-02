23439e8e5202e9bb90b1bd69a1a9c8706272b08e
Aliaksej Mialeshka <a2.meleshko@a1qa.com>
2025-12-02 14:11:50 +0100

    Update to Selenium v.4.38.0 +semver: feature - Update core version, update DevTools references to use V140

--- Aquality.Selenium/src/Aquality.Selenium/Browsers/DevToolsEmulationExtensions.cs
+++ Aquality.Selenium/src/Aquality.Selenium/Browsers/DevToolsEmulationExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using Aquality.Selenium.Core.Utilities;
 using OpenQA.Selenium.DevTools;
-using OpenQA.Selenium.DevTools.V140.DOM;
-using OpenQA.Selenium.DevTools.V140.Emulation;
+using OpenQA.Selenium.DevTools.V142.DOM;
+using OpenQA.Selenium.DevTools.V142.Emulation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
