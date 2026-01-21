5c95542eb1c502452e4255ede3683c2692626f36
Aliaksej Mialeshka <a2.meleshko@a1qa.com>
2026-01-21 14:50:21 +0100

    Update to Selenium 4.40.0 +semver:feature Update year of package and license Update DevTools references to use V144 in library and V143 in model-specific tests Stabilize tests, increase script timeout

--- Aquality.Selenium/src/Aquality.Selenium/Browsers/DevToolsPerformanceExtensions.cs
+++ Aquality.Selenium/src/Aquality.Selenium/Browsers/DevToolsPerformanceExtensions.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium.DevTools.V143.Performance;
+﻿using OpenQA.Selenium.DevTools.V144.Performance;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
