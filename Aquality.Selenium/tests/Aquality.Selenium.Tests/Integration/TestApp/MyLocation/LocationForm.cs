0ef0926753fe2dcdad20a0fff7d2d29313b258b1
Aliaksej Mialeshka <a2.meleshko@a1qa.com>
2026-01-21 16:11:28 +0100

    Update location form locators

--- Aquality.Selenium/tests/Aquality.Selenium.Tests/Integration/TestApp/MyLocation/LocationForm.cs
+++ Aquality.Selenium/tests/Aquality.Selenium.Tests/Integration/TestApp/MyLocation/LocationForm.cs
@@ -11,8 +11,8 @@ namespace Aquality.Selenium.Tests.Integration.TestApp.MyLocation
         private const string LocatorTemplate = "//div[@aria-expanded='true']//td[contains(., '{0}')]/following-sibling::td[not(contains(.,'waiting') or contains(.,'failed'))]";
         private readonly ILabel latitudeLabel = ElementFactory.GetLabel(By.XPath(string.Format(LocatorTemplate, "Latitude")), "Latitude");
         private readonly ILabel longitudeLabel = ElementFactory.GetLabel(By.XPath(string.Format(LocatorTemplate, "Longitude")), "Longitude");
-        private readonly IButton browserGeoLocationButton = ElementFactory.GetButton(By.XPath("//*[@aria-controls='geo-div']"), "Browser GeoLocation");
-        private readonly IButton startTestButton = ElementFactory.GetButton(By.Id("geo-test"), "Browser GeoLocation");
+        private readonly IButton browserGeoLocationButton = ElementFactory.GetButton(By.XPath("//button[contains(., 'Browser Geolocation')]"), "Browser GeoLocation");
+        private readonly IButton startTestButton = ElementFactory.GetButton(By.Id("geo-test"), "Start Test");
         private readonly IButton consentCookieInfoButton = ElementFactory.GetButton(By.XPath("//button[contains(@aria-label,'Consent')]"), "Consent cookie info");
         private readonly IButton gotItCookieButton = ElementFactory.GetButton(By.XPath("//a[contains(@aria-label,'dismiss')] | //img[contains(@src,'close')]"), "Got it! for Google cookie");
         private readonly IButton closeCookieSettingsButton = ElementFactory.GetButton(By.XPath("//*[contains(@class,'google-revocation-link')]//*[contains(@src,'close') or contains(@aria-label, 'lose')]"), "Close cookie settings");
